a22344a36c5e4847cb960e6b699f71b6be0f66f7
Anton Wijns <antonwijns@hotmail.com>
2014-05-05 19:35:32 +0200

    lay out changed

--- index.php
+++ index.php
@@ -17,17 +17,17 @@ include_once('classes/Owners.class.php');
  <body>
  	
 	<div id="login">
-		
+	<a href="index.php"><img src="images/logodik.png" alt="logoklein">	</a>
 	
 	<form action="<?php echo $_SERVER['PHP_SELF'];  ?>" method="post">
 
-	<label for="username">Gebruikersnaam</label>
+	<label for="username">Gebruikersnaam: </label>
 	<input type="text" id="username" name="username">
 
-	<p><label for="password">Paswoord</label>
+	<p><label for="password">Paswoord: </label>
 	<input type="password" id="password" name="password"></p>
 
-	<input type="submit" value="inloggen">
+	<input class="btn" type="submit" value="inloggen">
 	</form>
 
 	</div>
@@ -51,14 +51,15 @@ include_once('classes/Owners.class.php');
 		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 		<input type="text" name="name" placeholder="Naam" />
 		<input type="text" name="firstname" placeholder="Voornaam" />
-		<input type="text" name="street" placeholder="Straat + nr" />
+		<input type="text" name="street" placeholder="Straat" />
+		<input type="text" name="number" placeholder="Huisnummer" />
 		<input type="text" name="postcode" placeholder="Postcode" />
 		<input type="text" name="city" placeholder="Plaats" />
 		<input type="text" name="phonenumber" placeholder="Gsm-nummer" />
 		<input type="email" name="email" placeholder="E-mail" />
 		<input type="password" name="password" placeholder="Paswoord" />
 		
-		<input type="submit" name="btnSignup" value="Aanmelden" />
+		<input class="btn" type="submit" name="btnSignup" value="Aanmelden" />
 		</form>
 	</div> <!--end signup-->
 
